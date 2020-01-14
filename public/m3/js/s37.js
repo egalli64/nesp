139cec587a80e703c23099446a1542c01bb0b77f
Manny <egalli64@gmail.com>
2020-01-14 21:56:43 +0100

    oo examples

---
+++ public/m3/js/s37.js
@@ -0,0 +1,28 @@
+/**
+ * s37.js
+ */
+
+class PersonB {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    get fullName() {
+        return this.first + ' ' + this.last;
+    }
+
+    set fullName(name) {
+        let buffer = name.split(' ');
+        this.first = buffer[0];
+        this.last = buffer[1];
+    }
+}
+
+function log37(first, last) {
+    let p = new PersonB(first, last);
+    console.log(p.fullName);
+
+    p.fullName = 'Bob Hope';
+    console.log(p.fullName);
+}
---
+++ public/m3/js/s39.js
@@ -0,0 +1,30 @@
+/**
+ * s39.js
+ */
+
+class PersonD {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    fullInfo() {
+        return this.first + ' ' + this.last;
+    }
+}
+
+class Employee extends PersonD {
+    constructor(first, last, salary) {
+        super(first, last);
+        this.salary = salary;
+    }
+
+    fullInfo() {
+        return super.fullInfo() + ': ' + this.salary;
+    }
+}
+
+function log39(first, last, salary) {
+    let jon = new Employee(first, last, salary);
+    console.log(jon.fullInfo());
+}
