0c5b76fcb3d54c4e1083617c1a2b242db171ce01
Manny <egalli64@gmail.com>
2020-01-18 19:47:11 +0100

    hello ts

---
+++ m4/hello.ts
@@ -0,0 +1,5 @@
+function greetings(name: string) {
+    return "Hello, " + name;
+}
+
+console.log(greetings("TypeScript"));
---
+++ m4/out/hello.js
@@ -0,0 +1,6 @@
+"use strict";
+function greetings(name) {
+    return "Hello, " + name;
+}
+console.log(greetings("TypeScript"));
+//# sourceMappingURL=hello.js.map
