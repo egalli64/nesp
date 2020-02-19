1906dcdad2fda8aa9dcd4ce154fb807bd14e401c
Emanuele Galli <egalli64@gmail.com>
2020-02-19 15:41:40 +0100

    enabled cors

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let express = require('express');
+let cors = require('cors')
 let app = express();
 
+app.use(cors());
 app.use(express.static('public'));
+
+app.get('/hello', function (req, res) {
+    res.send('{"message": "Hello World!"}');
+});
 
 app.listen(3000, function () {
     console.log('Listening on port 3000');
