e42667565f2e29f77e3c4d95676e74e8b1638849
Manny <egalli64@gmail.com>
2020-06-29 13:58:05 +0200

    basic stuff on function

--- public/m3a/s15.js
+++ public/m3a/s15.js
@@ -2,26 +2,28 @@ function f() {
     console.log('hello from f()');
 }
 
-console.log('calling function f()');
-f();
-
-let x = function () {
-    console.log('hello from an anonymous function');
-}
-
-console.log('calling the anonymous function stored in x');
-x();
-
 function g(a, b) {
     return a + b;
 }
 
-console.log('passing numbers to function g() ...', g(2, 3));
+console.log('calling function f()');
+f();
 
-console.log('passing strings to function g() ...', g('Hello', 'World'));
+let result = g(3, 5);
+console.log('g(3, 5)', result);
 
+let fa = function () {
+    console.log('hello from an anonymous function');
+}
 
-let adder = (a, b) => a + b;
+console.log('calling the anonymous function stored in fa');
+fa();
 
-console.log('passing numbers to the anonymous function stored in adder ...', adder(2, 3));
-console.log('passing strings to the anonymous function stored in adder ...', adder('Hello', 'World'));
+let ga = function(a, b) {
+    return a + b;
+}
+
+console.log('passing numbers to the anonymous function stored in ga ...', ga(2, 3));
+console.log('passing strings to the anonymous function stored in ga ...', ga('Hello', 'World'));
+
+console.log("add a number to undefined and you get Not a Number ...", ga(42));
