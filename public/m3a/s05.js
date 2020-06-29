4a5a0d752031a536ff8b267c80ba5eaf0d56eead
Manny <egalli64@gmail.com>
2020-06-29 11:22:53 +0200

    floating point modulo example

--- public/m3a/s05.js
+++ public/m3a/s05.js
@@ -13,6 +13,10 @@ console.log('2 / 3 is', value);
 value = 2 % 3;
 console.log('2 % 3 is', value);
 
+value = 2.3 % 2.1;
+console.log('2.3 % 2.1 is', value);
+
+
 value = 2 ** 3;
 console.log('2**3 is', value);
 
