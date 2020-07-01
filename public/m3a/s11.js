d524f5b58f3b954463ac88df31996ea7c808fcdf
Manny <egalli64@gmail.com>
2020-07-01 12:47:36 +0200

    simple search in string

--- public/m3a/s11.js
+++ public/m3a/s11.js
@@ -1,12 +1,15 @@
 let s = 'Tom,Bob,Sally';
 console.log(s);
 console.log('length', s.length);
-console.log('first char', s[0]);
+console.log('first char', s[0], s.charAt(0));
 console.log('last char', s[s.length - 1]);
 
 let idx = s.indexOf('Bob');
 console.log("index of Bob", idx);
 console.log("index of Bill", s.indexOf('Bill'));
+console.log("Tom is at the beginning?", s.startsWith('Tom'));
+console.log("Sally is at the end?", s.endsWith('Sally'));
+console.log("Bob is in?", s.includes('Bob'));
 
 let end = s.indexOf(',', idx);
 console.log("next comma position", end);
