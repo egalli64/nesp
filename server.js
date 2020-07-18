243107b88bf5f48002145c56069ea9cbe3ccc447
Emanuele Galli <egalli64@gmail.com>
2020-07-18 17:41:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/hello', function (req, res) {
     res.send('{"message": "Hello World!"}');
 });
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000');
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
 });
