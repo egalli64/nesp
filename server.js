9316b2bb7c9f3007c8e105b4214b012d31f8d3aa
Emanuele Galli <egalli64@gmail.com>
2020-07-18 16:43:46 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let cors = require('cors');
+let app = express();
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/hello', function (req, res) {
+    res.send('{"message": "Hello World!"}');
+});
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
