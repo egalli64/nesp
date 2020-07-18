2892514e7cdee25dea861510e065b00357ccd78b
Manny <egalli64@gmail.com>
2020-07-18 18:27:16 +0200

    OpenShift compatibility

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.get('/hello', function (req, res) {
 });
 
 let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-let ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+let ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+app.listen(port, ip, function () {
+    console.log(`Listening on ${ip}:${port}`);
+});
