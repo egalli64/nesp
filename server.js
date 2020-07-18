c85fb73234ebcee09b3208481e7d75fba45bd6e2
Emanuele Galli <egalli64@gmail.com>
2020-07-18 17:58:04 +0200

    Using process

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.get('/hello', function (req, res) {
     res.send('{"message": "Hello World!"}');
 });
 
-app.listen(function () {
-    console.log('Nesp is listening');
-});
+let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+let ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
