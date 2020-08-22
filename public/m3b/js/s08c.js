59d106756bed9b400d0a2885e942d19e63c2434e
Manny <egalli64@gmail.com>
2020-08-22 21:44:52 +0200

    using Math.trunc()

--- public/m3b/js/s08c.js
+++ public/m3b/js/s08c.js
@@ -1,8 +1,17 @@
 /**
  * Used from the page, could be loaded before or after
  */
+function random(choice) {
+    // Math.trunc() is an ES 6 feature. Use floor() or maybe "| 0" for backward compability
+    return Math.trunc(Math.random() * choice);
+}
+
+let changeBackColorAmongThree = () => {
+    let colors = ['red', 'green', 'blue'];
+    document.getElementsByTagName('body')[0].style.backgroundColor = colors[random(colors.length)];
+};
 
 let changeBackColor = () => {
-    let colors = ['red', 'green', 'blue'];
-    document.getElementsByTagName('body')[0].style.backgroundColor = colors[(Math.random() * colors.length) | 0];
+    let color = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    document.getElementsByTagName('body')[0].style.backgroundColor = color;
 };
