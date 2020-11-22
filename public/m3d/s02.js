1f3c1c64d2f9532aa80bf9cdddb732d5a87e2d44
Manny <egalli64@gmail.com>
2020-11-22 17:44:57 +0100

    more log

--- public/m3d/s02.js
+++ public/m3d/s02.js
@@ -10,8 +10,9 @@ console.log("Saying hello to Bob and Bill, both in a second");
 setTimeout(sayHello, 1000, "Bob");
 let id = setTimeout(sayHello, 1000, "Bill");
 
-console.log("And to Tom every two seconds for five seconds");
+console.log("And to Tom every two seconds");
 let id2 = setInterval(sayHello, 2000, "Tom");
+console.log("Stopping to say hello to Tom in five seconds");
 setTimeout(clearInterval, 5000, id2);
 
 console.log("Changed my mind, say nothing to Bill");
