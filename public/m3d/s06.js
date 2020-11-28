a9022c7f3fa9ca4c5f54b9ac4cae5bca63adf9ae
Manny <egalli64@gmail.com>
2020-11-28 16:44:19 +0100

    cleanup

--- public/m3d/s06.js
+++ public/m3d/s06.js
@@ -1,47 +1,56 @@
 // How to create a promise via resolve()
 
-// resolve a value
-console.log("Resolve a value");
+console.log("(1) Resolve a value");
 
 let param = 42.42;
 
 Promise.resolve(param)
-    .then(res => console.log("resolved ->", res));
+    .then(res => console.log("(1) value resolved ->", res));
 
-// resolve a function that leads to a value
-console.log("Resolve a value /2");
+console.log("(2) Resolve a function returning a value");
 
 function multiply(a, b) {
-    console.log(`multiplying ${a} by ${b} ...`)
+    console.log(`Multiplying ${a} by ${b}`);
+    if (typeof a != 'number' || typeof b != 'number') {
+        // synchronous exception
+        throw new Error("Only numbers are accepted");
+    }
     return a * b;
 }
 
+try {
+    Promise.resolve(multiply(6, 'hello'))
+        .then(value => console.log("Not used in this example"))
+        .catch(err => console.log("Not used in this example"));
+} catch (err) {
+    console.log("(2a) If function throws, exception is managed synchronously ->", err.message);
+}
+
 Promise.resolve(multiply(6, 7))
-    .then(value => console.log("resolved (function) ->", value));
+    .then(value => console.log("(2b) Function resolved ->", value));
 
-console.log("A thenable object that resolves to a string")
-let wouldResolve = {
+console.log("(3) A fulfilled thenable")
+let goodThenable = {
     then: resolve => {
-        resolve(42);
-        console.log("After resolving wouldResolve.then() but before it crashes");
-        throw new Error("Something went wrong - but after resolving");
-        console.log("You should not see this message");
+        resolve(multiply(3, 7));
+        console.log("(3) After resolving then() but before it crashes");
+        throw new Error("After resolving something goes wrong");
+        console.log("Unreacheable code");
     }
 };
 
-Promise.resolve(wouldResolve)
-    .then(res => console.log("wouldResolve ->", res))
-    .catch(err => console.log("You should not see this message", err));
+Promise.resolve(goodThenable)
+    .then(res => console.log("(3) Thenable fulfilled ->", res))
+    .catch(err => console.log("Not used in this example"));
 
-console.log("A thenable object that throws an Error")
-let wouldFail = {
+console.log("(4) A rejected thenable")
+let badThenable = {
     then: resolve => {
-        throw new Error("Something went wrong");
-        console.log("You should not see this message");
-        resolve("resolved");
+        resolve(multiply(3, 'hello'));
+        console.log("Not used in this example");
     }
 };
 
-Promise.resolve(wouldFail)
-    .then(() => console.log("You should not see this message"))
-    .catch(err => console.log("wouldFail ->", err));
+Promise.resolve(badThenable)
+    .then(() => console.log("Not used in this example"))
+    .catch(err => console.log("(4) Thenable rejected ->", err.message));
