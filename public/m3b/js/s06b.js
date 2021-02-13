0d5c6958f2009ad9362731f3e00571c19bda632f
Emanuele Galli <egalli64@gmail.com>
2021-02-13 17:54:29 +0100

    alternative check on submit

--- public/m3b/js/s06b.js
+++ public/m3b/js/s06b.js
@@ -2,10 +2,22 @@
  * Bind a function on submit to an element -> it should be in the DOM!
  */
 
+// document.getElementById('second').onsubmit = () => {
+//     if (document.getElementById('y').value.length == 0) {
+//         console.log('y is empty!')
+//         return false;
+//     }
+//     return true;
+// };
+
 document.getElementById('second').onsubmit = () => {
-    if (document.getElementById('y').value.length == 0) {
-        console.log('y is empty!')
-        return false;
+    let y = document.getElementById('y');
+    let text = y.value;
+
+    if (text.length > 2) {
+        return true;
     }
-    return true;
+
+    console.log('y is too short!')
+    return false;
 };
