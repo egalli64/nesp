e224fb2194df399da98cafd2f424caf1ea03dd45
Emanuele Galli <egalli64@gmail.com>
2021-03-11 22:44:31 +0100

    simple request checker

--- server.js
+++ server.js
@@ -9,8 +9,22 @@ app.get('/hello', function (req, res) {
     res.send('{"message": "Hello World!"}');
 });
 
-let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-let ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+app.get('/test', function (req, res) {
+    console.log(req.query);
+
+    let result = '<h1>Parameters: name and value</h1>\n';
+    result += '<ul>\n'
+    for(let item in req.query) {
+        result += `<li>${item}: ${req.query[item]}</li>\n`;
+    }
+    result += '</ul>\n'
+
+    res.send(result);
+});
+
+
+let port = process.env.PORT || 8080;
+let ip = process.env.IP || 'localhost';
 
 app.listen(port, ip, function () {
     console.log(`Listening on ${ip}:${port}`);
