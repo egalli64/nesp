79573b2b22996c03acce65d8dc38a1cc72e20e17
Emanuele Galli <egalli64@gmail.com>
2021-07-08 21:35:20 +0200

    no onclick

--- public/m3b/js/s14.js
+++ public/m3b/js/s14.js
@@ -1,46 +1,51 @@
+// DOM assumed already loaded
+
 const APP_NAME = 'nesp';
 
+let localRadio = document.getElementById('local');
+let target = document.getElementById('target');
+let keyInput = document.getElementById('set');
+let valueInput = document.getElementById('value');
+let delInput = document.getElementById('del');
+let msgInput = document.getElementById('msg');
+
 function selectedStorage() {
-    return document.getElementById('local').checked ? localStorage : sessionStorage;
+    return localRadio.checked ? localStorage : sessionStorage;
 }
 
-function clearStorage() {
-    selectedStorage().clear();
-}
-
-function getAllItems() {
+document.getElementById('btnAll').addEventListener('click', function () {
     let storage = selectedStorage();
-    let target = document.getElementById('target');
     target.value += 'Number of items currently in selected storage: ' + storage.length + '\n';
     for (let i = 0; i < storage.length; i++) {
         let key = storage.key(i);
         target.value += key + ' -> ' + storage.getItem(key) + '\n';
     }
-}
+});
 
-function setItem() {
-    let key = APP_NAME + '.' + document.getElementById('set').value;
-    let value = document.getElementById('value').value;
+document.getElementById('btnClear').addEventListener('click', function () {
+    selectedStorage().clear();
+});
+
+document.getElementById('btnSet').addEventListener('click', function () {
+    let key = APP_NAME + '.' + keyInput.value;
+    let value = valueInput.value;
     console.log('set', key, value);
     selectedStorage().setItem(key, value);
-}
+});
 
-function removeItem() {
-    let key = APP_NAME + '.' + document.getElementById('del').value;
+document.getElementById('btnDel').addEventListener('click', function () {
+    let key = APP_NAME + '.' + delInput.value;
     console.log('remove', key);
     selectedStorage().removeItem(key);
-}
+});
 
-function setMessage() {
+document.getElementById('btnMsg').addEventListener('click', function () {
     let key = APP_NAME + '.message';
     let obj = {
         recipient: "Bob",
-        msg: document.getElementById('msg').value
+        msg: msgInput.value
     };
 
     console.log('set', key, obj);
-    selectedStorage().setItem(key, JSON.stringify({
-        recipient: 'Bob',
-        msg: document.getElementById('msg').value
-    }));
-}
+    selectedStorage().setItem(key, JSON.stringify(obj));
+});
