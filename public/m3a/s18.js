8fbe74b1f2d0c24d163da1bc579127eb12ed35c8
Emanuele <egalli64@gmail.com>
2022-12-03 15:48:26 +0100

    month comment

--- public/m3a/s18.js
+++ public/m3a/s18.js
@@ -1,8 +1,13 @@
 /**
- * Date
+ * @file The Date object
  */
 
+// the current datetime
 let now = new Date();
-
 console.log(now);
+
+// extracting components from a date - notice the month!
 console.log("Today is " + now.getDate() + ", month " + (now.getMonth() + 1));
+
+// create a date passing year, month (zero based!), day, ...
+console.log("January is month zero!", new Date(2023, 0, 1, 11, 37, 51));
