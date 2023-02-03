21da18d2477edb6dc3f9e872dc8f9982456c3380
Emanuele <egalli64@gmail.com>
2023-02-03 11:30:52 +0100

    more examples

--- public/m3a/s17.js
+++ public/m3a/s17.js
@@ -2,7 +2,16 @@
  * Math
  */
 
-console.log('three interesting numbers:', Math.E, Math.SQRT2, Math.PI);
+console.log('three interesting numbers:', Math.E, Math.SQRT2, Math.PI)
 
-console.log('biggest one:', Math.max(Math.E, Math.SQRT2, Math.PI));
-console.log('smallest one:', Math.min(Math.E, Math.SQRT2, Math.PI));
+console.log('biggest one:', Math.max(Math.E, Math.SQRT2, Math.PI))
+console.log('smallest one:', Math.min(Math.E, Math.SQRT2, Math.PI))
+
+console.log('Pi rounded', Math.round(Math.PI))
+console.log('E rounded', Math.round(Math.E))
+
+console.log('Pi to ceil', Math.ceil(Math.PI))
+console.log('E to ceil', Math.ceil(Math.E))
+
+console.log('Pi to floor', Math.floor(Math.PI))
+console.log('E to floor', Math.floor(Math.E))
