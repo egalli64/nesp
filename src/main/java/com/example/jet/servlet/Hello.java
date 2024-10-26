7d723ac701c9bb79f1b25fdd2201ce0fe7ac6a50
Emanuele <egalli64@gmail.com>
2024-10-26 14:13:16 +0200

    jetty server support

---
+++ src/main/java/com/example/jet/servlet/Hello.java
@@ -0,0 +1,17 @@
+package com.example.jet.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet {
+    private static final long serialVersionUID = 4190906381967108924L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello!</h1>");
+    }
+}
