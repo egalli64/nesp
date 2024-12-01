3f9532cfa94306eab3f0da3992ff3b3bd22e0fe3
Emanuele <egalli64@gmail.com>
2024-12-01 19:08:54 +0100

    review

--- public/m3/c/script/s7.js
+++ public/m3/c/script/s7.js
@@ -1,7 +1,14 @@
+/*
+ * Introduction to Web technologies
+ * 
+ * JavaScript - addEventListener()
+ * 
+ * https://github.com/egalli64/web-intro
+ */
+
 /**
  * Bind a function on submit to an element -> it should be in the DOM!
  */
-
 document.getElementById('second').addEventListener('submit', event => {
     if (document.getElementById('y').value.length == 0) {
         console.log('y is empty!');
