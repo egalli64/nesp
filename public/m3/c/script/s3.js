5d89823a267853476898d18109e011ed29190d80
Emanuele <egalli64@gmail.com>
2024-12-01 14:25:58 +0100

    simplify

--- public/m3/c/script/s3.js
+++ public/m3/c/script/s3.js
@@ -7,6 +7,15 @@
  */
 
 /**
+ * Requires an element with id target to be already present in the DOM
+ */
+let target = document.getElementById('target');
+
+target.value += `window heights: ${window.outerHeight}, ${window.innerHeight}\n`;
+target.value += `window widths: ${window.outerWidth}, ${window.innerWidth}\n`;
+
+
+/**
  * Example for the confirm() function in the BOM
  */
 function checkConfirm() {
